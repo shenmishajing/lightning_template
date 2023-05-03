7539f8c25018eda40c8362c440fef4a08b28a486
shenmishajing <shenmishajing@gmail.com>
2023-05-03 13:31:55 +0800

    fix import bug, bump version to 1.0.8

--- lightning_template/__init__.py
+++ lightning_template/__init__.py
@@ -1,4 +1,7 @@
 from . import *
-from .datasets import *
-from .models import *
-from .tools import *
+from .datasets import LightningDataModule as LightningDataModule
+from .models import LightningModule as LightningModule
+from .tools import batch_size_finder as batch_size_finder
+from .tools import cli as cli
+from .tools import lr_finder as lr_finder
+from .tools import model_statistics as model_statistics
--- lightning_template/tools/__init__.py
+++ lightning_template/tools/__init__.py
@@ -1,2 +1,4 @@
 from . import *
-from .model import *
+from .model import batch_size_finder as batch_size_finder
+from .model import lr_finder as lr_finder
+from .model import model_statistics as model_statistics
