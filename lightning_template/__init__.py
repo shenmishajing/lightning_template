de0c1bd102f06eece42a8936ba6cff814bce1e94
shenmishajing <shenmishajing@gmail.com>
2023-05-08 16:50:24 +0800

    remove import in tools

--- lightning_template/__init__.py
+++ lightning_template/__init__.py
@@ -1,7 +1,3 @@
 from . import *
 from .datasets import LightningDataModule as LightningDataModule
 from .models import LightningModule as LightningModule
-from .tools import batch_size_finder as batch_size_finder
-from .tools import cli as cli
-from .tools import lr_finder as lr_finder
-from .tools import model_statistics as model_statistics
--- lightning_template/tools/__init__.py
+++ lightning_template/tools/__init__.py
@@ -1,4 +1,1 @@
 from . import *
-from .model import batch_size_finder as batch_size_finder
-from .model import lr_finder as lr_finder
-from .model import model_statistics as model_statistics
