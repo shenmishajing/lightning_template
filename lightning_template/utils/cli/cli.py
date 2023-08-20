5e62eeded81cc8353e55838a1c9bc1eef48bc581
shenmishajing <shenmishajing@gmail.com>
2023-08-20 16:20:23 +0800

    add ns to default id

--- lightning_template/utils/cli/cli.py
+++ lightning_template/utils/cli/cli.py
@@ -1,6 +1,6 @@
+import datetime
 import os
 import sys
-import time
 from types import MethodType
 from typing import Any, Callable, Dict, Optional, Tuple, Type, Union
 
@@ -84,7 +84,7 @@ class LightningCLI(_LightningCLI):
             else self.config[self.config["subcommand"]]
         )
         name = os.path.splitext(os.path.split(config["config"][0].abs_path)[1])[0]
-        version = time.strftime("%Y%m%d_%H%M%S", time.localtime(time.time()))
+        version = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S.%f")
         if (
             config.get("trainer") is not None
             and config["trainer"].get("logger") is not None
