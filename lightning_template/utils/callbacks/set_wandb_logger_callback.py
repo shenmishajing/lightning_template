e0f1dc6a6ee0c9c81846b1cc87d65a52002a03d6
shenmishajing <shenmishajing@gmail.com>
2023-09-28 18:54:51 +0800

    add disabel log code and watch model feature

--- lightning_template/utils/callbacks/set_wandb_logger_callback.py
+++ lightning_template/utils/callbacks/set_wandb_logger_callback.py
@@ -1,5 +1,5 @@
-from functools import partial
-from typing import Optional
+import re
+from typing import Optional, Union
 
 from lightning.pytorch import Callback, LightningModule, Trainer
 from lightning.pytorch.loggers.wandb import WandbLogger
@@ -10,28 +10,35 @@ class SetWandbLoggerCallback(Callback):
 
     def __init__(
         self,
-        log_code_cfg: Optional[dict] = None,
-        watch_model_cfg: Optional[dict] = None,
+        log_code_cfg: Optional[Union[dict, bool]] = None,
+        watch_model_cfg: Optional[Union[dict, bool]] = None,
     ):
         self.log_code_cfg = log_code_cfg if log_code_cfg is not None else {}
         self.watch_model_cfg = watch_model_cfg if watch_model_cfg is not None else {}
 
-        default_log_code_cfg = {
-            "name": "code",
-            "include_fn": [".py", ".yaml", ".yml", ".sh", ".md"],
-        }
-        for key, value in default_log_code_cfg.items():
-            self.log_code_cfg.setdefault(key, value)
-        for name in ["include_fn", "exclude_fn"]:
-            if name in self.log_code_cfg:
-                self.log_code_cfg[name] = partial(
-                    lambda path, exts: any([path.endswith(ext) for ext in exts]),
-                    exts=self.log_code_cfg[name],
-                )
+        if isinstance(self.log_code_cfg, dict):
+            default_log_code_cfg = {
+                "name": "code",
+                "include_fn": [
+                    re.Pattern(p) for p in [".py", ".yaml", ".yml", ".sh", ".md"]
+                ],
+                "exclude_fn": [
+                    re.Pattern(p) for p in ["data/", "work_dirs/", "wandb/"]
+                ],
+            }
+            for key, value in default_log_code_cfg.items():
+                self.log_code_cfg.setdefault(key, value)
+            for name in ["include_fn", "exclude_fn"]:
+                if name in self.log_code_cfg:
+                    self.log_code_cfg[name] = lambda path: any(
+                        p.match(path) is not None for p in self.log_code_cfg[name]
+                    )
 
     def setup(
         self, trainer: Trainer, pl_module: LightningModule, stage: Optional[str] = None
     ) -> None:
         if trainer.logger is not None and isinstance(trainer.logger, WandbLogger):
-            trainer.logger.watch(model=pl_module, **self.watch_model_cfg)
-            trainer.logger.experiment.log_code(**self.log_code_cfg)
+            if isinstance(self.watch_model_cfg, dict):
+                trainer.logger.watch(model=pl_module, **self.watch_model_cfg)
+            if isinstance(self.log_code_cfg, dict):
+                trainer.logger.experiment.log_code(**self.log_code_cfg)
