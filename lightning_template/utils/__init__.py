a2dbd3b4d3f5941c01480267f496a81f6a4e0d9a
shenmishajing <shenmishajing@gmail.com>
2024-01-07 10:00:47 +0800

    chore(dependency): remove the unnecessary dependencies

--- lightning_template/utils/__init__.py
+++ lightning_template/utils/__init__.py
@@ -1,7 +1,3 @@
-from speed_benchmark.speed_benchmark import speed_benchmark as speed_benchmark
-from speed_benchmark.visualization import draw_line_chart as draw_line_chart
-from speed_benchmark.visualization import ncolors as ncolors
-
 from .cli import LightningCLI as LightningCLI
 from .cli import Trainer as Trainer
 from .timer import Timer as Timer
