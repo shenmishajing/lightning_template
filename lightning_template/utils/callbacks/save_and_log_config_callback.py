3429873a7152bb5781a2799f3e91b045bd5779c3
shenmishajing <shenmishajing@gmail.com>
2024-01-16 22:13:04 +0800

    refactor(SaveAndLogConfigCallback): move log_hyperparams to save_config func

--- lightning_template/utils/callbacks/save_and_log_config_callback.py
+++ lightning_template/utils/callbacks/save_and_log_config_callback.py
@@ -1,5 +1,3 @@
-from typing import Optional
-
 from lightning.pytorch import LightningModule, Trainer
 from lightning.pytorch.cli import SaveConfigCallback
 
@@ -7,9 +5,23 @@ from lightning.pytorch.cli import SaveConfigCallback
 class SaveAndLogConfigCallback(SaveConfigCallback):
     """Saves and logs a LightningCLI config to the log_dir when training starts."""
 
-    def setup(
-        self, trainer: Trainer, pl_module: LightningModule, stage: Optional[str] = None
+    def save_config(
+        self, trainer: Trainer, pl_module: LightningModule, stage: str
     ) -> None:
-        super().setup(trainer, pl_module, stage)
+        """Implement to save the config in some other place additional to the standard
+        log_dir.
+
+        Example:
+            def save_config(self, trainer, pl_module, stage):
+                if isinstance(trainer.logger, Logger):
+                    config = self.parser.dump(self.config, skip_none=False)  # Required for proper reproducibility
+                    trainer.logger.log_hyperparams({"config": config})
+
+        Note:
+            This method is only called on rank zero. This allows to implement a custom save config without having to
+            worry about ranks or race conditions. Since it only runs on rank zero, any collective call will make the
+            process hang waiting for a broadcast. If you need to make collective calls, implement the setup method
+            instead.
+        """
         if trainer.logger is not None:
             trainer.logger.log_hyperparams(self.config.as_dict())
