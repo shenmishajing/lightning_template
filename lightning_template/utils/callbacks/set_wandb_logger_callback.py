881bf55b3efe894e870db901bae8030087383373
shenmishajing <shenmishajing@gmail.com>
2024-01-18 20:35:45 +0800

    fix(wandb-log-code): fix use code_dir does not follow link bug, use glob instead of walk

--- lightning_template/utils/callbacks/set_wandb_logger_callback.py
+++ lightning_template/utils/callbacks/set_wandb_logger_callback.py
@@ -1,10 +1,13 @@
 import os
 import re
 from functools import partial
+from glob import iglob
 from typing import Optional, Union
 
 from lightning.pytorch import Callback, LightningModule, Trainer
 from lightning.pytorch.loggers.wandb import WandbLogger
+
+import wandb
 
 
 class SetWandbLoggerCallback(Callback):
@@ -19,29 +22,73 @@ class SetWandbLoggerCallback(Callback):
         self.watch_model_cfg = watch_model_cfg if watch_model_cfg is not None else {}
 
         if isinstance(self.log_code_cfg, dict):
+            self.log_code_cfg.setdefault("name", "code")
             default_log_code_cfg = {
-                "name": "code",
-                "root": "code_dirs" if os.path.exists("code_dirs") else ".",
-                "include_fn": [
-                    re.compile(p)
-                    for p in ["\.py$", "\.yaml$", "\.yml$", "\.toml$", "\.sh$", "\.md$"]
+                "include_dirs": ["src", "tests", "tools", "docs", "configs", ".github"],
+                "include_file_types": [
+                    "py",
+                    "yaml",
+                    "yml",
+                    "toml",
+                    "sh",
+                    "md",
+                    "gitignore",
+                    "ini",
                 ],
-                "exclude_fn": [
-                    re.compile(p)
-                    for p in ["data/", "work_dirs/", "wandb/", "__pycache__/"]
-                ],
+                "exclude_patterns": ["__pycache__"],
             }
             for key, value in default_log_code_cfg.items():
-                self.log_code_cfg.setdefault(key, value)
-            for name in ["include_fn", "exclude_fn"]:
-                if name in self.log_code_cfg:
+                if key not in self.log_code_cfg:
+                    self.log_code_cfg[key] = value
+                else:
+                    if not isinstance(self.log_code_cfg[key], list):
+                        self.log_code_cfg[key] = [self.log_code_cfg[key]]
+                    self.log_code_cfg[key].extend(value)
 
-                    def _is_match(path, root=None, patterns=None):
-                        return any(p.search(path) is not None for p in patterns)
+            include_patterns = []
+            for type in self.log_code_cfg["include_file_types"]:
+                for dir_name in self.log_code_cfg["include_dirs"]:
+                    include_patterns.append(f"{dir_name}/**/*.{type}")
+                include_patterns.append(f"*.{type}")
+            include_patterns.extend(["LICENSE*", "license*"])
 
-                    self.log_code_cfg[name] = partial(
-                        _is_match, patterns=self.log_code_cfg[name]
+            self.log_code_cfg["exclude_patterns"] = [
+                re.compile(p) for p in self.log_code_cfg["exclude_patterns"]
+            ]
+
+            def _is_match(path, patterns=None):
+                return any(p.search(path) is not None for p in patterns)
+
+            self.log_code_cfg["include_patterns"] = include_patterns
+            self.log_code_cfg["exclude_fn"] = partial(
+                _is_match, patterns=self.log_code_cfg["exclude_patterns"]
+            )
+
+    def log_code(self, trainer: Trainer):
+        art = wandb.Artifact(self.log_code_cfg["name"], "code")
+        files_added = False
+        for glob_pattern in self.log_code_cfg["include_patterns"]:
+            for file_path in iglob(
+                glob_pattern,
+                root_dir=self.log_code_cfg.get("root"),
+                include_hidden=True,
+                recursive=True,
+            ):
+                if not self.log_code_cfg["exclude_fn"](file_path):
+                    files_added = True
+                    save_name = (
+                        file_path
+                        if self.log_code_cfg.get("root") is None
+                        else os.path.relpath(file_path, self.log_code_cfg["root"])
                     )
+                    art.add_file(file_path, name=save_name)
+        if not files_added:
+            wandb.termwarn(
+                "No relevant files were detected in the specified directory. No code will be logged to your run."
+            )
+            return None
+
+        return trainer.logger.experiment.log_artifact(art)
 
     def setup(
         self, trainer: Trainer, pl_module: LightningModule, stage: Optional[str] = None
@@ -50,4 +97,4 @@ class SetWandbLoggerCallback(Callback):
             if isinstance(self.watch_model_cfg, dict):
                 trainer.logger.watch(model=pl_module, **self.watch_model_cfg)
             if isinstance(self.log_code_cfg, dict):
-                trainer.logger.experiment.log_code(**self.log_code_cfg)
+                self.log_code(trainer)
